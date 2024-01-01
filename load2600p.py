620460e5949a3bff6a8593741a15fc01b3963039
Mr.Blinky <mstr.blinky@gmail.com>
2024-01-01 17:50:37 +0100

    update load2600p.py
    add support for firmware 1.1 allowing more 2600 rom sizes (multiple of 1K)
    longer no cart delay

--- load2600p.py
+++ load2600p.py
@@ -1,5 +1,5 @@
 ################################################################################
-#  Atari 2600+ ROM loader v1.01                        by Mr. Blinky Dec 2023  #
+#  Atari 2600+ ROM loader v1.02               by Mr. Blinky Dec 2023- Jan 2024 #
 ################################################################################
 
 # requires Python 3 & pyserial to be installed
@@ -10,7 +10,9 @@ import serial
 import os.path
 import time
 
-print('Atari 2600+ ROM loader v1.00 by Mr. Blinky Dec 2023\n')
+FIRMWARE_VERSION = '1.1' # Atari2600+ firmware version
+
+print('Atari 2600+ ROM loader v1.02 by Mr. Blinky Dec 2023\n')
 print('Using python version {}\n'.format(platform.python_version()))
 if  (len(sys.argv) < 3):
     print('USAGE: {} comport romfile'.format(os.path.basename(sys.argv[0])))
@@ -21,9 +23,9 @@ filename = sys.argv[2]
 
 try:
     com = serial.Serial(port, 115800, timeout = None, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, rtscts = 0)
-    com.setDTR(True)  # use DTR or RTS (depends on which pin is used)
-    com.setRTS(True)  # wil set CAR_TEST low to signal now cart
-    time.sleep(.1)
+    com.dtr = 1  # use DTR or RTS (depends on which pin is available)
+    com.rts = 1  # wil set CARD_TEST low to signal no cart
+    time.sleep(.5)
     com.write(b'\xEA')
     time.sleep(.5)
 except serial.SerialException:
@@ -41,11 +43,14 @@ if a7800:
   checksize = 3 + romsize + 128
 else:
   romsize = len(rom)
-  if romsize == 2048:
+  if romsize == 2048 and FIRMWARE_VERSION == '1.0': # FW v1.0 needs 2K roms expanded to 4K
       rom = rom + rom
       romsize += romsize
+  elif romsize == 10495: # for Pitfall II: only keep 8K rom and 2K gfx data (rom must be a multiple of 1K)
+     rom = rom[:10240]
+     romsize = 10240
   checksize = 3 + romsize
-  if not romsize in [4096, 8192, 16384, 32768, 65536]  :
+  if (not romsize in [4096, 8192, 16384, 32768, 65536] and FIRMWARE_VERSION == '1.0') or (romsize % 1024) :
       print("Error rom size not supported")
       sys.exit()
 #create serial data      
@@ -61,8 +66,8 @@ for i in range(checksize):
 data += bytearray([0x55, 0xAA, checksum, 0, 0])
 #write serial data
 print('Loading Atari2600+ with {} ROM: {}'.format(7800 if a7800 else 2600, os.path.basename(filename)))
-com.setDTR(False) # use DTR or RTS (depends on which pin is used)
-com.setRTS(False) # set car_TEST high to signal cart inserted
+com.dtr = 0 # use DTR or RTS (depends on which pin is available)
+com.rts = 0 # set CARD_TEST high to signal cart inserted
 time.sleep(.1)
 com.write(data)
-com.close() #keeps CAR_TEST high (DTR/RTS false)
+com.close() #keeps CARD_TEST high (DTR/RTS false) and cart inserted
